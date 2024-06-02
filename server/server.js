1f287e9187db67285993a7275ff37c196d86254e
Clay Stewart <c.stewart89@gmail.com>
2024-06-02 00:14:22 -0400

    Setup front end client

--- server/server.js
+++ server/server.js
@@ -47,5 +47,5 @@ app.use(errorHandler);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
